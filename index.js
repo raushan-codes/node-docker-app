bbf67be0b5d409cf0a99d76ddbe553dd5baf2d85
Raushan Kumar <raushan@ineuron.ai>
2023-10-29 01:27:04 +0530

    changed msg again 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res)=>{
     res.status(200).json({
-        message: 'short message from the server. runner is great'
+        message: 'short message from the server. runner is great. check yourself'
     });
 });
 
