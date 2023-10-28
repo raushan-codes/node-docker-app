4b4d48ffdf56d107fcd0788a3de006b3d75bcba0
Raushan Kumar <raushan@ineuron.ai>
2023-10-29 00:42:27 +0530

    modified response

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res)=>{
     res.status(200).json({
-        message: 'welcome to the server'
+        message: 'this response is from the EC2 server running a docker image (container)'
     });
 });
 
